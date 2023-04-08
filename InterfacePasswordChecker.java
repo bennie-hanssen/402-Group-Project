a0adf97a37bc3a06321a6d5fb94312cfa413a212
veronicalupinacci <116969384+veronicalupinacci@users.noreply.github.com>
2023-04-08 16:51:12 -0400

    Added outputStrenght string
    Added outputStrenght string and various comments

---
+++ InterfacePasswordChecker.java
@@ -0,0 +1,16 @@
+/**
+ * Interface for the passwordChecker
+ * 
+ * @version March 30, 2023
+ */
+
+public interface InterfacePasswordChecker {
+	// abstract methods
+	public boolean checkLenght();
+
+	public boolean checkSpecialCharacter();
+
+	public boolean checkUpperCase();
+
+	public boolean checkNumber();
+}
