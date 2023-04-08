ff586bff67955c15f0e4b9cb9767284cc4ec4813
Veronica L <vlupinacci02@gmail.com>
2023-04-08 18:15:16 -0400

    Revert "Added outputStrenght string"
    This reverts commit a0adf97a37bc3a06321a6d5fb94312cfa413a212.

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -1,134 +1,14 @@
 /**
- * Password checker class
- * 
- * @version Apr 8, 2023
- * 
+ * Interface for the passwordChecker
  */
-public class PasswordChecker implements InterfacePasswordChecker {
-	String password;
-	int score;
-	boolean resultLenght, resultSpecial, resultUpper, resultNumber, resultEmptySpace;
-	String specialCharactersString = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
 
-	/**
-	 * Constructor for CheckPassword
-	 * 
-	 * @param
-	 * String password
-	 * 
-	 */
-	public PasswordChecker(String password) {
-		this.password = password;
-		score = 0;
-	}
+public interface PasswordChecker {
+	// abstract methods
+	public boolean checkLenght();
 
-	/**
-	 * Method to check the length.
-	 * 
-	 * @return
-	 * true if the length is at least 12
-	 * 
-	 */
-	public boolean checkLenght() {
-		// check if the length of the string is at least 12
-		if (password.length() >= 12) {
-			// add the length to the score
-			score += password.length();
-			// set resultLenght to true
-			resultLenght = true;
-		}
-		else {
-			resultLenght = false;
-		}
-		return resultLenght;
-	}
+	public boolean checkSpecialCharacter();
 
-	/**
-	 * Method to check if there is a special character.
-	 * 
-	 * @return
-	 * true if there is a special character
-	 * 
-	 */
-	public boolean checkSpecialCharacter() {
-		for (int i = 0; i < password.length(); i++) {
-	        if (password.matches("(?=.*[!@#$%^&*()]).*")){
-				resultSpecial = true;
-			} else {
-				resultSpecial = false;
-			}
-		}
-		score++;
-		return resultSpecial;
-	}
+	public boolean checkUpperCase();
 
-	/**
-	 * Method to check if there is at least a number.
-	 * 
-	 * @return
-	 * true if there is a number
-	 */
-	public boolean checkNumber() {
-		for (int i = 0; i < password.length(); i++) {
-            if (password.matches("(?=.*[0-9]).*")){
-	            resultNumber = true;
-			} else {
-				resultNumber = false;
-			}
-		}
-		score++;
-		return resultNumber;
-	}
-	
-	/**
-	 * Method to check if the string has at least an empty space or is empty
-	 * 
-	 * @return 
-	 * true if there is an empty space
-	 * 
-	 */
-	public boolean checkEmptySpace() {
-		boolean emptyString = password.isBlank();
-		boolean containsEmptySpace = password.contains(" ");
-		for (int i = 0; i < password.length(); i++) {
-            if (emptyString == true  || containsEmptySpace == true){
-	            resultEmptySpace = true;
-	    		score --;
-
-			} else {
-				resultEmptySpace = false;
-			}
-		}
-		return resultEmptySpace;
-	}
-
-
-	/**
-	 * Method to check if there is at least an upper case character.
-	 * 
-	 * @return
-	 * true when there is an upper case character
-	 * 
-	 */
-	public boolean checkUpperCase() {
-		for (int i = 0; i < password.length(); i++) {
-            if (password.matches("(?=.*[A-Z]).*")){
-	            resultUpper = true;
-			} else {
-				resultUpper = false;
-			}
-			}
-		score++;
-		return resultUpper;
-		}
-	
-	/**
-	 * 
-	 * @return
-	 * int score
-	 * 
-	 */
-	public int getScore() {
-		return score;
-	}
+	public boolean checkNumber();
 }
